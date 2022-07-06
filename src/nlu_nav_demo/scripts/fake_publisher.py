85bddf99268f3ef0adfa9f00a337e4529e640f54
JinWoo Kim <59794238+kjwoo31@users.noreply.github.com>
2022-07-06 20:03:53 +0900

    Update fake_publisher.py

--- src/nlu_nav_demo/scripts/fake_publisher.py
+++ src/nlu_nav_demo/scripts/fake_publisher.py
@@ -16,23 +16,26 @@ class FakePublish:
       self.bbox=None
       self.detectimg=None
       self.trackimg=None
-      self.map=None
+    #   self.map=None
       self.text=None
+      self.scan=None
 
 
       # Publishers
       self.bbox_pub = rospy.Publisher("/detector/bbox_lines", Marker, queue_size=1)
       self.detectimg_pub = rospy.Publisher("/annotated_image", Image, queue_size=1)
       self.trackimg_pub = rospy.Publisher("/tracker/map_image", Image, queue_size=1)
-      self.map_pub = rospy.Publisher("/map", OccupancyGrid, queue_size=10)
+    #   self.map_pub = rospy.Publisher("/map", OccupancyGrid, queue_size=1)
       self.text_pub = rospy.Publisher("/tracker/text", OverlayText, queue_size=1)
+      self.scan_pub = rospy.Publisher("/haetae/scan_merged", LaserScan, queue_size=50)
 
       # Subscribers
       rospy.Subscriber("/detector/bbox_lines", Marker, self.bbox_callback)
       rospy.Subscriber("/annotated_image", Image, self.detectimg_callback)
       rospy.Subscriber("/tracker/map_image", Image, self.trackimg_callback)
-      rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
+    #   rospy.Subscriber("/map", OccupancyGrid, self.map_callback)
       rospy.Subscriber("/tracker/text", OverlayText, self.text_callback)
+      rospy.Subscriber("/haetae/scan_merged", LaserScan, self.scan_callback)
 
     def bbox_callback(self, msg):
         self.bbox = msg
@@ -49,6 +52,9 @@ class FakePublish:
     def text_callback(self, msg):
         self.text = msg
 
+    def scan_callback(self, msg):
+        self.scan = msg
+
     def run(self):
         while not rospy.is_shutdown():
             try:
@@ -63,14 +69,18 @@ class FakePublish:
                 self.trackimg_pub.publish(self.trackimg)
             except:
                 print("trackimg not publishing yet")
-            #try:  
-            #    self.map_pub.publish(self.map_pub)
-            #except:
-            #    print("map not publishing yet")                
+            # try:  
+            #     self.map_pub.publish(self.map_pub)
+            # except:
+            #     print("map not publishing yet")                
             try:  
                 self.text_pub.publish(self.text)
             except:
                 print("text not publishing yet")                
+            try:  
+                self.scan_pub.publish(self.scan)
+            except:
+                print("scan not publishing yet")  
 
 
 if __name__ == '__main__':
